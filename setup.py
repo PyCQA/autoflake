95109688910b61b3eeab4b47b50a20254647e2b4
myint <macbookpro.local>
2012-12-27 00:10:58 -0800

    Automatically install dependencies if necessary

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 """Setup for autoflake."""
 
-from distutils import core
+import sys
 
 
 def version():
@@ -13,19 +13,49 @@ def version():
                 return ast.literal_eval(line.split('=')[1].strip())
 
 
+def pyflakes_installed():
+    """Return True if pyflakes executable is installed."""
+    import os
+    import subprocess
+    try:
+        process = subprocess.Popen(
+            ['pyflakes', os.devnull],
+            stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+        process.communicate()
+        return True
+    except OSError:
+        return False
+
+
 with open('README.rst') as readme:
-    core.setup(name='autoflake',
-               version=version(),
-               description='Removes unused imports.',
-               long_description=readme.read(),
-               license='Expat License',
-               author='myint',
-               url='https://github.com/myint/autoflake',
-               classifiers=['Intended Audience :: Developers',
-                            'Environment :: Console',
-                            'Programming Language :: Python :: 2.7',
-                            'Programming Language :: Python :: 3',
-                            'License :: OSI Approved :: MIT License'],
-               keywords='clean,automatic,unused,import',
-               py_modules=['autoflake'],
-               scripts=['autoflake'])
+    setup_arguments = {
+        'name': 'autoflake',
+        'version': version(),
+        'description': 'Removes unused imports.',
+        'long_description': readme.read(),
+        'license': 'Expat License',
+        'author': 'myint',
+        'url': 'https://github.com/myint/autoflake',
+        'classifiers': ['Intended Audience :: Developers',
+                        'Environment :: Console',
+                        'Programming Language :: Python :: 2.7',
+                        'Programming Language :: Python :: 3',
+                        'License :: OSI Approved :: MIT License'],
+        'keywords': 'clean,automatic,unused,import',
+        'py_modules': ['autoflake'],
+        'scripts': ['autoflake'],
+    }
+
+
+# Don't bother trying to install pyflakes3k on 3.3. It is not compatible.
+if pyflakes_installed() or sys.version_info >= (3, 3):
+    from distutils import core
+    core.setup(**setup_arguments)
+else:
+    # Only resort to setuptools if necessary.
+    setup_arguments['install_requires'] = [
+        'pyflakes' if sys.version_info[0] < 3 else 'pyflakes3k']
+
+    import setuptools
+    setuptools.setup(**setup_arguments)
