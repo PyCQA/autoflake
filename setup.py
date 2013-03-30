97b994b455e1f0b0d2a8c6a01217728647ae339c
Steven Myint <git@stevenmyint.com>
2013-03-29 19:06:55 -0700

    Simplify

--- setup.py
+++ setup.py
@@ -2,11 +2,8 @@
 """Setup for autoflake."""
 
 import ast
-import sys
 
-
-if sys.version_info < (2, 7):
-    raise SystemExit('autoflake requires Python >= 2.7')
+import setuptools
 
 
 def version():
@@ -17,47 +14,22 @@ def version():
                 return ast.parse(line).body[0].value.s
 
 
-def pyflakes_installed():
-    """Return True if pyflakes executable is installed."""
-    import os
-    import subprocess
-    try:
-        process = subprocess.Popen(
-            ['pyflakes', os.devnull],
-            stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-
-        process.communicate()
-        return True
-    except OSError:
-        return False
-
-
 with open('README.rst') as readme:
-    setup_arguments = {
-        'name': 'autoflake',
-        'version': version(),
-        'description': 'Removes unused imports.',
-        'long_description': readme.read(),
-        'license': 'Expat License',
-        'author': 'Steven Myint',
-        'url': 'https://github.com/myint/autoflake',
-        'classifiers': ['Intended Audience :: Developers',
-                        'Environment :: Console',
-                        'Programming Language :: Python :: 2.7',
-                        'Programming Language :: Python :: 3',
-                        'License :: OSI Approved :: MIT License'],
-        'keywords': 'clean,automatic,unused,import',
-        'py_modules': ['autoflake'],
-        'scripts': ['autoflake'],
-    }
-
-
-if pyflakes_installed():
-    from distutils import core
-    core.setup(**setup_arguments)
-else:
-    # Only resort to setuptools if necessary.
-    import setuptools
     setuptools.setup(
+        name='autoflake',
+        version=version(),
+        description='Removes unused imports.',
+        long_description=readme.read(),
+        license='Expat License',
+        author='Steven Myint',
+        url='https://github.com/myint/autoflake',
+        classifiers=['Intended Audience :: Developers',
+                     'Environment :: Console',
+                     'Programming Language :: Python :: 2.7',
+                     'Programming Language :: Python :: 3',
+                     'License :: OSI Approved :: MIT License'],
+        keywords='clean,automatic,unused,import',
+        py_modules=['autoflake'],
+        scripts=['autoflake'],
         install_requires=['pyflakes'],
-        **setup_arguments)
+        test_suite='test_autoflake')
