f846bf4d9cf3f4c1562ea842f458fe6fb159ce6e
Steven Myint <git@stevenmyint.com>
2014-04-25 06:24:02 -0700

    Handle case with commas better
    This relaxes cdc2d8738fe3ccb8490f3889748b4dddf43dd1ef.

--- autoflake.py
+++ autoflake.py
@@ -282,10 +282,12 @@ def filter_unused_variable(line, previous_line=''):
         return re.sub(r' as \w+:$', ':', line, count=1)
     elif multiline_statement(line, previous_line):
         return line
-    elif line.count('=') == 1 and ',' not in line:
+    elif line.count('=') == 1:
         split_line = line.split('=')
         assert len(split_line) == 2
         value = split_line[1].lstrip()
+        if ',' in split_line[0]:
+            return line
 
         if is_literal_or_name(value):
             value = 'pass' + get_line_ending(line)
