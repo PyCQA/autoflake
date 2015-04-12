6e698225ef19437b7921c8aa0ead9988b2c1a10f
Steven Myint <git@stevenmyint.com>
2015-04-12 09:18:55 -0700

    Add Python 3.5

--- setup.py
+++ setup.py
@@ -31,6 +31,7 @@ with open('README.rst') as readme:
                      'Programming Language :: Python :: 3.2',
                      'Programming Language :: Python :: 3.3',
                      'Programming Language :: Python :: 3.4',
+                     'Programming Language :: Python :: 3.5',
                      'Topic :: Software Development :: Quality Assurance'],
         keywords='clean,fix,automatic,unused,import',
         py_modules=['autoflake'],
