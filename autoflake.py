bbe52bc0b9319c740edbc1981f09ef3dec513ef8
Steven Myint <git@stevenmyint.com>
2018-01-12 10:39:56 -0800

    Avoid multiline values when removing duplicate keys

--- autoflake.py
+++ autoflake.py
@@ -484,14 +484,22 @@ def filter_duplicate_key(line, message, line_number, marked_line_numbers,
 
 
 def dict_entry_has_key(line, key):
-    """Return True if `line` is a dict entry that uses `key`."""
-    result = re.match(r'\s*(.*)\s*:.*,?\s*$', line)
+    """Return True if `line` is a dict entry that uses `key`.
+
+    Return False for multiline cases where the line should not be removed by
+    itself.
+
+    """
+    result = re.match(r'\s*(.*)\s*:\s*(.*),?\s*$', line)
     if not result:
         return False
 
     try:
         candidate_key = ast.literal_eval(result.group(1))
     except (SyntaxError, ValueError):
+        return False
+
+    if multiline_statement(result.group(2)):
         return False
 
     return candidate_key == key
